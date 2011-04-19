693a45aa791626966413fcd89476c8b043be8777
Michael McAtee <jmmcatee@gmail.com>
2011-04-19 14:01:54 -0400

    Basic user interface mechanism created

--- _user_interface.h
+++ _user_interface.h
@@ -1,12 +1,107 @@
-void printOptions();
+/*
+ *	This is a library for the user interface portion of wirePirate.
+ */
+
+int printOptions();
+int runOptions(int option);
+void runRandGrabPacket();
+int runGrabPacket();
 
 
-
-void printOptions()
+int printOptions()
 {
+	int option = 0;
+	
+	fflush( stdout );
 	printf("Please choose an option.\n");
-	printf("1. Grab packet or frame\n");
-	printf("2. Grab packet and frame stack\n");
-	printf("3. Get a number of packets and frames (not implemented)\n");
-	printf("->");
+	printf(" 1: Grab random packet and frame stack\n");
+	printf(" 2: Grab specific type packet and frame stack\n");
+	printf(" 3: Get a number of packets and frames (not implemented)\n");
+	printf(" q: Quit");
+	printf(" -> "); fflush( stdout );
+	
+	scanf("%d", &option);
+	printf("\n");
+	
+	return option;
+}
+
+int runOptions(int option)
+{
+	switch(option)
+	{
+		case 1:
+			runRandGrabPacket();
+			return 1;
+			break;
+		case 2:
+			while ( runGrabPacket() ) {/* will run until completed successfully */}
+			return 1;
+			break;
+		case 3:
+			printf("Does nothing\n");
+			return 1;
+			break;
+		case 0:
+			printf("\nExiting...\n\n");
+			return 0;
+			break;
+		default:
+			printf("Option %d is not a valid choice. Please select again\n", option);
+			return 1;
+	}
+}
+
+void runRandGrabPacket()
+{
+	int s = createSocket();
+	unsigned char buffer[ETH_FRAME_LEN] = "";
+	struct ethernetFrame *randomFrame;
+	
+	printf("Capturing Packets...\n\n");
+	randomFrame = parseFrame( buffer, getLinkLayerFrame(s, buffer) );
+	printFrame(randomFrame);
+}
+
+int runGrabPacket()
+{
+	int frameType = 0;
+	
+	int s = createSocket();
+	unsigned char buffer[ETH_FRAME_LEN] = "";
+	struct ethernetFrame *frameWithType;
+	
+	printf("Please select the EtherType you wish to grab.\n");
+	printf(" 1: ARP Frames  (0x0809)\n");
+	printf(" 2: IPv4 & IPv6 (0x0800)\n");
+	printf(" 0: Quit\n");
+	printf(" -> "); fflush(stdout);
+	
+	scanf("%d", &frameType);
+	printf("\n");
+	
+	switch(frameType)
+	{
+		case 1:
+			do {
+				frameWithType = parseFrame( buffer, getLinkLayerFrame(s, buffer) );
+			} while ( !checkEtherType(frameWithType, arpType) );
+			printFrame(frameWithType);
+			return 1;
+			break;
+		case 2:
+			do {
+				frameWithType = parseFrame( buffer, getLinkLayerFrame(s, buffer) );
+			} while ( !checkEtherType(frameWithType, ip4Type) );
+			printFrame(frameWithType);
+			return 1;
+			break;
+		case 0:
+			return 0;
+			break;
+		default:
+			printf("Error, %d is not a valid option. Please choose again.\n", frameType);
+			return 1;
+			break;
+	}
 }
--- wirePirate.c
+++ wirePirate.c
@@ -17,34 +17,9 @@
 
 
 int main()
-{	
-	unsigned char buffer[ETH_FRAME_LEN] = "";	/* unsigned (important) byte element size array of ethernet octet length (1514) */
-	struct ethernetFrame *ip4Frame;
-	struct ethernetFrame *arpFrame;
-	
-	printOptions();
-	
-	int s = createSocket();
-	
-	printf("Capturing Packets...\n\n");
-
-	/* Get ARP Frame */
-	unsigned long int frameCount = 1;
-	do {
-		printf("Frame Count: %ld\r", frameCount); fflush( stdout );
-		arpFrame = parseFrame(buffer, getLinkLayerFrame(s, buffer));
-		frameCount++;
-	} while( !checkEtherType(arpFrame, arpType) );
-	printf("\n");
-	printFrame(arpFrame);
-	
-	/* Get IPv4 Frame */
-	frameCount = 1;
-	do {
-		printf("Frame Count: %ld\r", frameCount); fflush( stdout );
-		ip4Frame = parseFrame(buffer, getLinkLayerFrame(s, buffer));
-		frameCount++;
-	} while( !checkEtherType(ip4Frame, ip4Type) );
-	printf("\n");
-	printFrame(ip4Frame);
+{
+	while ( runOptions(printOptions()) )
+	{
+		// This will run until it is successful or exited
+	}
 }
